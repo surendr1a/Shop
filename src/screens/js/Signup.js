06fa3aac39ca3a423563c558c41452dffb3a0dc7
surendr1a <105473099+surendr1a@users.noreply.github.com>
2024-09-15 12:12:05 +0530

    Update Signup.js

--- src/screens/js/Signup.js
+++ src/screens/js/Signup.js
@@ -10,7 +10,7 @@ export default function Signup() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/api/createuser", {
+    const response = await fetch("shop-production-09d5.up.railway.app/api/createuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
